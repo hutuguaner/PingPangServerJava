d8141f60aa465c6f35b33d8caee48bd58af6a287
lizeqi <553761200@qq.com>
2019-12-16 23:18:17 +0800

    hehe

--- src/com/pingpang/Main.java
+++ src/com/pingpang/Main.java
@@ -1,10 +1,37 @@
 package com.pingpang;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-			
+		int port = 555333;
+		ServerSocket serverSocket = new ServerSocket(port);
+		
+		Socket clientSocket = serverSocket.accept();
+		MyReceiveClientThread myReceiveClientThread = new MyReceiveClientThread(clientSocket);
+		myReceiveClientThread.start();
+		
+	}
+	
+	
+	private static class MyReceiveClientThread extends Thread{
+		
+		Socket clientSocket;
+
+		public MyReceiveClientThread(Socket clientSocket) {
+			super();
+			this.clientSocket = clientSocket;
+		}
+		
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			super.run();
+		}
 	}
 
 }
